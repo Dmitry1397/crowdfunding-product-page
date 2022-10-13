dcf0f80a86bcf792f971d7c0ab90bc8e0cffacef
Dmitry1397 <dyshik8@gmail.com>
2022-10-13 16:03:04 +0200

    fixing mobile menu scroll

--- menu.js
+++ menu.js
@@ -5,8 +5,10 @@ menuBtn.addEventListener('click', () => {
     if (nav.style.display === 'block') {
         nav.style.display = 'none';
         menuBtn.className = 'open';
+        document.body.style.overflow = 'unset';
     } else {
         nav.style.display = 'block';
         menuBtn.className = 'close';
+        document.body.style.overflow = 'hidden';
     }
 })
